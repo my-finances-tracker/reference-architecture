e877a75c8d265231fdb967471ae5dbcd620243a0
Lennard Claproth <l.claproth@octobox.nl>
2024-02-12 15:08:29 +0100

    First setup structure

---
+++ src/MyFinancesTracker.Transactions.Domain/Entities/Contracts/V1/Command/CreateFromCsvCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFinancesTracker.Transactions.Domain.Entities.Contracts.V1.Command;
+internal class CreateFromCsvCommand
+{
+}
---
+++ src/MyFinancesTracker.Transactions.Domain/Entities/DbModel/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFinancesTracker.Transactions.Domain.Entities.DbModel
+{
+    internal class Transaction
+    {
+    }
+}
